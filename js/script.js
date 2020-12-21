409a0db128689ea60c02e15e03fffb35077d6a5a
Teresa Spigoni <teresaspigoni@gmail.com>
2020-12-21 19:06:37 +0100

    bozza 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // ingredienti
 var ingredients = document.getElementsByClassName('ingredients')[0].getElementsByTagName('input');
 
+// nome del burger
 var burgerName = document.getElementsByClassName('burger-name')[0];
 
 // variabile prezzo
@@ -9,18 +10,27 @@ var price = 2;
 // array codici sconto
 var discount = ['XMASDISCOUNT2020', 'REFUND2020', 'DISCOUNT20'];
 
+// codice dell'uetnte
+var userDiscount = document.getElementById('discount');
+
 // evento click
 document.getElementById('button').addEventListener("click", function() {
   var price = 2;
-
-  if (burgerName.value === '') {
+  if (burgerName.value === '') { //se non inserisco il nome al burger alert
     alert('Inserisci un nome per il tuo burger')
-  } else {
+  } else { //calcolo del prezzo senza sconto
     for (var i = 0; i < ingredients.length; i++) {
       if (ingredients[i].checked) {
         price += parseInt(ingredients[i].value);
+
+        if (userDiscount.value === discount[i]) { //calcolo del prezzo con sconto(...)
+          price = (price / 100) * 80;
+        }
       }
     }
+    //stampo il prezzo
     document.getElementById('price').innerHTML = price;
   }
 });
+
+// TODO: Cprezzo con lo sconto e almeno due checkbox true.
