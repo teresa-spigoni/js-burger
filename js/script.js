afd4933421046f8c59b0ed1e835d26bb7b7cd998
Teresa Spigoni <teresaspigoni@gmail.com>
2020-12-21 18:27:19 +0100

    nome burger obbligatorio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
+// ingredienti
 var ingredients = document.getElementsByClassName('ingredients')[0].getElementsByTagName('input');
+
+var burgerName = document.getElementsByClassName('burger-name')[0];
+
+// variabile prezzo
 var price = 2;
 
+// array codici sconto
+var discount = ['XMASDISCOUNT2020', 'REFUND2020', 'DISCOUNT20'];
+
+// evento click
 document.getElementById('button').addEventListener("click", function() {
   var price = 2;
-  for (var i = 0; i < ingredients.length; i++) {
-    if (ingredients[i].checked) {
-      price += parseInt(ingredients[i].value);
+
+  if (burgerName.value === '') {
+    alert('Inserisci un nome per il tuo burger')
+  } else {
+    for (var i = 0; i < ingredients.length; i++) {
+      if (ingredients[i].checked) {
+        price += parseInt(ingredients[i].value);
+      }
     }
+    document.getElementById('price').innerHTML = price;
   }
-  document.getElementById('price').innerHTML = price;
 });
