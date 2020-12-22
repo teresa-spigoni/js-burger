cfa7ed793e3bea99a5177073ef99b69abefb0854
Teresa Spigoni <teresaspigoni@gmail.com>
2020-12-22 16:37:09 +0100

    js comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// costanti ------------------------------------------------------
+// constant ------------------------------------------------------
 var minIngredients = 2;
 var burgerBaseCost = 2;
 var discount = 0.8;
@@ -6,41 +6,45 @@ var discountCoupon = ['XMASDISCOUNT2020', 'REFUND2020', 'DISCOUNT20'];
 // ---------------------------------------------------------------
 
 
+// burger name, ingredients, price and discount ------------------
 var burgerName = document.getElementsByClassName('burger-name')[0];
 var ingredients = document.getElementsByClassName('ingredients')[0].getElementsByTagName('input');
-
 var price = document.getElementById('price');
 var userDiscount = document.getElementById('discount');
+// ----------------------------------------------------------------
 
 
+// counters -------------------------------------------------------
 var counter = 0;
 var cost = 0;
+// ----------------------------------------------------------------
 
 
-// evento click
+// click event ----------------------------------------------------
 document.getElementById('button').addEventListener("click", function() {
-
+  // reset counters
   counter = 0;
   cost = burgerBaseCost;
 
-  for(var i = 0; i < ingredients.length; i++) {
+  for(var i = 0; i < ingredients.length; i++) {  //cycle checked ingredients to obtain partial price
+    //for each checked checkbox add 1 to the counter and add the value of the ingredient to the cost
     if(ingredients[i].checked) {
       counter++;
       cost += parseInt(ingredients[i].value);
     }
   }
 
-  if(burgerName.value.length === 0) {
+  if(burgerName.value.length === 0) {  //if there is no burger name
     alert('Scegli un nome per il tuo burger!')
-  } else if (counter < minIngredients) {
+  } else if (counter < minIngredients) {  //if there are less than 2 checked checkboxes
     alert('Seleziona almeno ' + minIngredients + ' ingredienti.')
   } else {
-    if(discountCoupon.indexOf(userDiscount.value) !== -1) {
+    if(discountCoupon.indexOf(userDiscount.value) !== -1) {  //apply the discount
       cost = cost * discount;
     }
 
   }
-
-  cost = cost.toFixed(2);
-  price.innerText = '$ ' + cost;
+  cost = cost.toFixed(2);  //obtain a number of only two decimal places
+  price.innerText = '$ ' + cost;  //print the final price
 });
+// ----------------------------------------------------------------
