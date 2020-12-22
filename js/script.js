2eb806318e146ff93cd9c66144f119573203df59
Teresa Spigoni <teresaspigoni@gmail.com>
2020-12-22 12:32:16 +0100

    validazione coupon e almeno 2 ingredienti

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
-// ingredienti
+// costanti ------------------------------------------------------
+var minIngredients = 2;
+var burgerBaseCost = 2;
+var discount = 0.8;
+var discountCoupon = ['XMASDISCOUNT2020', 'REFUND2020', 'DISCOUNT20'];
+// ---------------------------------------------------------------
+
+
+var burgerName = document.getElementsByClassName('burger-name')[0];
 var ingredients = document.getElementsByClassName('ingredients')[0].getElementsByTagName('input');
 
-// nome del burger
-var burgerName = document.getElementsByClassName('burger-name')[0];
-
-// variabile prezzo
-var price = 2;
-
-// array codici sconto
-var discount = ['XMASDISCOUNT2020', 'REFUND2020', 'DISCOUNT20'];
-
-// codice dell'uetnte
+var price = document.getElementById('price');
 var userDiscount = document.getElementById('discount');
+
+
+var counter = 0;
+var cost = 0;
+
 
 // evento click
 document.getElementById('button').addEventListener("click", function() {
-  var price = 2;
-  if (burgerName.value === '') { //se non inserisco il nome al burger alert
-    alert('Inserisci un nome per il tuo burger')
-  } else { //calcolo del prezzo senza sconto
-    for (var i = 0; i < ingredients.length; i++) {
-      if (ingredients[i].checked) {
-        price += parseInt(ingredients[i].value);
 
-        if (userDiscount.value === discount[i]) { //calcolo del prezzo con sconto(...)
-          price = (price / 100) * 80;
-        }
-      }
+  counter = 0;
+  cost = burgerBaseCost;
+
+  for(var i = 0; i < ingredients.length; i++) {
+    if(ingredients[i].checked) {
+      counter++;
+      cost += parseInt(ingredients[i].value);
     }
-    //stampo il prezzo
-    document.getElementById('price').innerHTML = price;
   }
-});
 
-// TODO: Cprezzo con lo sconto e almeno due checkbox true.
+  if(burgerName.value.length === 0) {
+    alert('Scegli un nome per il tuo burger!')
+  } else if (counter < minIngredients) {
+    alert('Seleziona almeno ' + minIngredients + ' ingredienti.')
+  } else {
+    if(discountCoupon.indexOf(userDiscount.value) !== -1) {
+      cost = cost * discount;
+    }
+
+  }
+  price.innerText = '$ ' + cost;
+});
